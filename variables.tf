a437859a1282c83c46c48bbabf4a247e5d187ec2
Lewisjmp <lewis.munoz@cuemby.com>
2024-09-10 15:40:49 -0500

    Nginx Walrus

--- variables.tf
+++ variables.tf
@@ -1,25 +1,17 @@
-#
-# Contextual Fields
-#
+variable "release_name" {
+  description = "The name of the Helm release."
+  type        = string
+  default     = "ingress-nginx"
+}
 
-variable "context" {
-  description = <<-EOF
-Receive contextual information. When Walrus deploys, Walrus will inject specific contextual information into this field.
+variable "namespace" {
+  description = "The namespace where the Helm release will be installed."
+  type        = string
+  default     = "kube-system"
+}
 
-Examples:
-```
-context:
-  project:
-    name: string
-    id: string
-  environment:
-    name: string
-    id: string
-  resource:
-    name: string
-    id: string
-```
-EOF
-  type        = map(any)
-  default     = {}
+variable "version" {
+  description = "The version of the ingress-nginx Helm chart."
+  type        = string
+  default     = "4.11.2"
 }
